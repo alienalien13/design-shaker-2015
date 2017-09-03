1bb30121634e95be2baa3fe3de113a006731f106
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-09-03 21:17:04 +0200

    this year

--- src/script.js
+++ src/script.js
@@ -5,7 +5,13 @@ $(function(){
 	$('.menu--button').on('click', function(){
 
 		$('.home-menu').toggleClass('nav-hide');
-		
+
+	})
+
+	$('.button-slide-down').on('click', function(ev){
+
+		$(window).scrollTop( $(ev.target).parent().position().top + $(ev.target).parent().innerHeight() )
+
 	})
 
 })
